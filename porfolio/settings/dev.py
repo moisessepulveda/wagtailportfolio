ae264793bae85600ecbfb1f783ef10fddb5c926c
moises <mo.sepulvedar@gmail.com>
2022-01-05 12:39:12 -0300

    first commit

---
+++ porfolio/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-b@4n=!y#@^=n+###t^g#ra9$5%ya7u5yyyia2@(an3^n!-quhd'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
